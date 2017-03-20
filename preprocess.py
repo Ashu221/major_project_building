12edc0cb5fdf95dcd6976f723a5d2ef197ba45ef
Ashu221 <linkingashu@gmail.com>
2017-03-20 17:38:33 +0545

    Add files via upload

---
+++ preprocess.py
@@ -0,0 +1,57 @@
+import matplotlib.pyplot as plt
+import fileinput
+import matplotlib.animation as animation
+
+To_be_replaced = ["playerTransformPositions: x = ", " z = "]
+
+filename = input("Type the file name also file format if anyother file format except .txt")
+X_buf = []
+Y_buf = []
+
+
+
+#Formating file name
+if '.' in filename:
+	fullfilename  = filename
+else :
+	fullfilename = filename +'.txt' 
+
+print("opening file  ", fullfilename);
+
+#Handling File 
+with open(fullfilename, 'r') as openfile:
+	if openfile!=[]:
+		print(fullfilename ," opened Successful")
+		for line in openfile:
+			line = line.replace(To_be_replaced[0],'')
+			line = line.replace(To_be_replaced[1],'')
+			#print(line)
+			data = line.split(',')
+			X_buf.append(float(data[0]))
+			Y_buf.append(float(data[1]))
+	else:
+			print("Error opening file")
+openfile.close()
+print("File closed")
+
+
+# #print(Y_buf,X_buf)
+# plt.plot(Y_buf,X_buf,'r-')
+# plt.title('test')
+# plt.show()
+
+
+#animation
+fig, ax = plt.subplots()
+line, = ax.plot(Y_buf,X_buf)
+
+def update(num, X_buf, Y_buf, line):
+	line.set_data(Y_buf[:num], X_buf[:num])
+	line.axes.axis([60, 180, 140, 260])
+	return line,
+
+ani = animation.FuncAnimation(fig, update, len(X_buf), fargs=[X_buf, Y_buf, line], interval = 1, blit=True)
+plt.show()
+
+
+
